51aeb478cf9d0d42f4576f7185c52e6100876a7f
Michael Pearce <mdpearce@gmail.com>
2023-06-26 14:37:55 +1000

    Add command line options for specifying input and output

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,38 @@ import * as fs from "fs";
 import {OpenApiSchema} from "./openApiSchema";
 import {registerTypes} from "./registerTypes";
 import {processSchemas} from "./processSchemas";
+import yargs, {option} from "yargs";
+import {hideBin} from "yargs/helpers";
+
+interface CommandLineOptions {
+    clientDir: string,
+    outputFile: string,
+}
+
+const argv = yargs(hideBin(process.argv))
+    .option('client-dir', {
+        alias: 'c',
+        description: "Specify the base directory for the lemmy JS client. Example: /Users/username/dev/lemmy-js-client",
+        type: "string",
+        default: ".",
+    })
+    .option('output-file', {
+        alias: 'o',
+        description: "A file to write the resultant Open API json schema to",
+        type: "string",
+        default: "./openapi.json",
+    })
+    .help()
+    .alias('h', 'help')
+    .argv as CommandLineOptions
+
+const inputClientDir: String = argv.clientDir
+const typesPath = inputClientDir + "/src/types/*.ts"
+const outputFile: string = argv.outputFile;
 
 const project = new Project();
-project.addSourceFilesAtPaths("/Users/michael/Development/lemmy-js-client/src/types/*.ts");
+console.log(`Loading files at ${typesPath})`)
+project.addSourceFilesAtPaths(typesPath);
 
 const openAPISchema: OpenApiSchema = {
     openapi: "3.0.0",
@@ -29,4 +58,4 @@ processSchemas(project.getSourceFiles(), typeRegistry, openAPISchema)
 
 console.log(JSON.stringify(openAPISchema, null, 2));
 
-fs.writeFileSync('openapi.json', JSON.stringify(openAPISchema, null, 2))
+fs.writeFileSync(outputFile, JSON.stringify(openAPISchema, null, 2))
