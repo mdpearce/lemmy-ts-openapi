bd59f08f39102cee152bd204cd046c07983e5a12
Michael Pearce <mdpearce@gmail.com>
2023-07-04 20:23:48 +1000

    Don't use request bodies - use request params

--- src/models/pathSchema.ts
+++ src/models/pathSchema.ts
@@ -1,3 +1,5 @@
 export type PathSchema = {
     $ref: string;
+} | {
+    type: string
 };
--- src/paths/processPaths.ts
+++ src/paths/processPaths.ts
@@ -1,13 +1,23 @@
 import {SourceFile} from "ts-morph";
 import {PathSchema} from "../models/pathSchema";
+import {mapTsTypeToJsonSchemaType} from "../utils/mapTsTypeToJsonSchemaType";
+import {isReferenceType} from "../utils/isReferenceType";
 
-export function processPaths(sourceFile: SourceFile): { [path: string]: any } {
+export function processPaths(sourceFile: SourceFile, typeRegistry: {
+    [name: string]: boolean
+}): { [path: string]: any } {
     const httpTypeRegex = /`HTTP\.(\w+) (.*)`/;  // regex to extract HTTP method and path from JSDoc comment
 
     function createSchemaRef(name: string): PathSchema {
         return {
             $ref: `#/components/schemas/${name}`
         };
+    }
+
+    function createSchemaPrimitive(typeName: string): PathSchema {
+        return {
+            type: mapTsTypeToJsonSchemaType(typeName)
+        }
     }
 
     const paths: { [path: string]: any } = {};
@@ -17,9 +27,6 @@ export function processPaths(sourceFile: SourceFile): { [path: string]: any } {
     for (const method of lemmyHttpClass.getMethods()) {
         // Extract the name of the method
         const operationId = method.getName();
-        if (operationId === "deleteAccount") {
-            console.log("foo")
-        }
 
         // Extract the JSDoc comment
         const comment = method.getJsDocs()[0]?.getDescription();
@@ -47,18 +54,34 @@ export function processPaths(sourceFile: SourceFile): { [path: string]: any } {
             throw new Error("Could not determine return type")
         }
 
+        param.getChildren()[2].getType().getProperties()[0].getDeclarations()[0]
+
+        const pathParams = param.getTypeNodeOrThrow().getType().getProperties().map(prop => {
+            const declaredProp = prop.getDeclarations()[0]
+            const isOptional = declaredProp.getSymbolOrThrow().isOptional()
+            const name = declaredProp.getSymbolOrThrow().getName()
+            const typeName = declaredProp.getType().getText()
+
+            let schemaType: PathSchema
+            if (isReferenceType(declaredProp.getType(), typeRegistry)) {
+                schemaType = createSchemaRef(typeName)
+            } else {
+                schemaType = createSchemaPrimitive(typeName)
+            }
+
+            return {
+                in: "query",
+                name: name,
+                required: !isOptional,
+                schema: schemaType
+            }
+        })
+
         // Create a path item for this method
         paths[path] = {
             [httpMethod]: {
                 operationId,
-                requestBody: {
-                    required: true,
-                    content: {
-                        'application/json': {
-                            schema: createSchemaRef(paramType),
-                        },
-                    },
-                },
+                parameters: pathParams,
                 responses: {
                     '200': {
                         description: 'OK',
